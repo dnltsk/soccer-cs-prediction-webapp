6a46fb48d9403ae0096b2785db826a3e195caa44
Daniel Teske <daniel.teske@meteogroup.com>
2016-06-07 16:32:20 +0200

    trigger two differnet java executables

--- bin/warmup.sh
+++ bin/warmup.sh
@@ -1,6 +1,17 @@
+# GOOD
+# OpenJDK Runtime Environment (build 1.8.0_92-cedar14-b14)
+# /app/.jdk/bin/java
+
+# BAD
+#OpenJDK Runtime Environment (IcedTea 2.6.6) (7u101-2.6.6-0ubuntu0.14.04.1)
+#/usr/bin/java
 until $(curl -o /dev/null -s -I -f http://localhost:$PORT); do
+	echo "### DEFAULT ###"
 	echo `java -version`
-	echo `which java`
+	echo "### 1.8 ###"
+	echo `/app/.jdk/bin/java -version`
+	echo "### 1.7 ###"
+	echo `/usr/bin/java -version`
   	echo "http://localhost:$PORT"
   	sleep 1
 done
